ca399c8155ac6812f4670895378404a1fda567cc
Austin Shelby <helloaustinshelby@gmail.com>
2022-11-13 18:47:49 +0100

    new logic

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,14 +1,25 @@
-import { ChangeEvent, FormEvent, useState } from "react";
+import { ChangeEvent, FormEvent, Fragment, useState } from "react";
 
 const regions = [
   {
     name: "Singapore",
     url: "https://faas-sgp1-18bc02ac.doserverless.co/api/v1/web/fn-c70d76cc-9eea-459c-8e09-73003a6b8d79/default/ping-website",
   },
+  {
+    name: "New York",
+    url: "https://faas-nyc1-2ef2e6cc.doserverless.co/api/v1/web/fn-d3627ee8-5865-429f-9444-34c626260179/pings/new-york-pink",
+  },
+  {
+    name: "London",
+    url: "https://faas-lon1-917a94a7.doserverless.co/api/v1/web/fn-2276c8b9-7f4d-4165-a749-af9b8996cb37/pings/london-ping",
+  },
 ];
+
+type Result = { region: string; durationMs: number };
 
 const HomePage = () => {
   const [url, setUrl] = useState<string>("");
+  const [results, setResults] = useState<Result[]>([]);
 
   const change = (event: ChangeEvent<HTMLInputElement>): void => {
     event.preventDefault();
@@ -17,36 +28,45 @@ const HomePage = () => {
 
   const submit = async (event: FormEvent) => {
     event.preventDefault();
-    const res = await fetch(
-      "https://faas-sgp1-18bc02ac.doserverless.co/api/v1/web/fn-c70d76cc-9eea-459c-8e09-73003a6b8d79/default/ping-website",
-      {
-        method: "POST",
-        body: JSON.stringify({ url }),
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
-      }
+    const results = await Promise.all(
+      regions.map(async (region) => {
+        const result = await fetch(region.url, {
+          method: "POST",
+          body: JSON.stringify({ url }),
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+        });
+        const json = await result.json();
+        return { region: region.name, durationMs: json.durationMs };
+      })
     );
-    console.log(res);
+    setResults(results);
   };
-  // https://faas-fra1-afec6ce7.doserverless.co/api/v1/web/fn-ace62185-5a20-425b-b754-3640ffa6708e/default/test-function
-  // https://faas-fra1-afec6ce7.doserverless.co/api/v1/web/fn-ace62185-5a20-425b-b754-3640ffa6708e/default/test-function
-
-  // const main = async () => {
-  //   const website = "https://stackoverflow.com/questions/29775797/fetch-post-json-data"
-  //   const start = process.hrtime.bigint()
-  //   const result = await fetch(website)
-  //   const end = process.hrtime.bigint()
-  //   return {"result": `Request took: ${((end - start) / BigInt(1_000_000)).toString()}ms`}
-  // }
 
   return (
     <div>
       <form onSubmit={submit}>
         <label htmlFor="url">URL</label>
         <input type="text" value={url} onChange={change} />
+        <button>Test Website Speed</button>
       </form>
+
+      <h2>Results</h2>
+      <dl>
+        {results.map((result, index) => (
+          <Fragment key={index}>
+            <dt>{result.region}</dt>
+            <dd>
+              {new Intl.NumberFormat("en-US", {
+                style: "unit",
+                unit: "millisecond",
+              }).format(result.durationMs)}
+            </dd>
+          </Fragment>
+        ))}
+      </dl>
     </div>
   );
 };
